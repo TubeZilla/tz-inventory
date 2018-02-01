b6d8b7843fe93098933820d2c3275f392070d510
archana <archana.nayak@gmail.com>
2018-01-31 22:53:58 -0800

    change to insert user data

--- database/index.js
+++ database/index.js
@@ -4,22 +4,22 @@ var Promise = require('bluebird');
 
 //params is an array of json objects that will be need to be mapped
 //to each users record column
-const createUser = (params, start, end, callback) => {
-  let t = process.hrtime();
-  let inserts = [];
-  for (var i = start; i < end; i++) {
-    // console.log(params[i]);
-    let insert = {username: params[i][0], email: params[i][1], userid: params[i][2]};
-    inserts.push(insert);
-  }
+const createUser = (params, callback) => {
+  // let t = process.hrtime();
+  // let inserts = [];
+  // for (var i = start; i < end; i++) {
+  //   // console.log(params[i]);
+  //   // let insert = {username: params[i][0], email: params[i][1], userid: params[i][2]};
+  //   inserts.push(params[i]);
+  // }
   
-  knex.insert(inserts).into('users')
+  knex.insert(params).into('users')
   .then((id) => {
     console.log(id);
     callback(null, id);
   });
-  t = process.hrtime(t);
-  console.log('time taken to execute user insertions', t[0], t[1]);
+  // t = process.hrtime(t);
+  // console.log('time taken to execute user insertions', t[0], t[1]);
 };
 
 //params is an array of json that has already been mapped
--- datagenerators/dataGeneratorHelper.js
+++ datagenerators/dataGeneratorHelper.js
@@ -1,14 +1,85 @@
 const fs = require('fs');
-const uuidv4 = require('uuid/v4'); 
+// const uuidv4 = require('uuid/v4'); 
 const path = require('path');
 const basePath = path.join(__dirname, '../datagenerators/');
 const Promise = require('bluebird');
+const faker = require('faker');
+var randomstring = require("randomstring");
+const db = require('../database/index.js');
 
+const generateUsers = (number, callback) => {
+  console.log('in generateUsers');
+  var t = process.hrtime();
+  var n = 1000;
+  while(n-- >= 0) {
+    generateUserData((err, users) => {
+      // writeFile(path.join(basePath, 'users' + count + '.json'), JSON.stringify(users));
+      // writeFile(path.join(basePath, 'userids' + count + '.json'), JSON.stringify(userIds));
+      db.createUser(users, (err, id) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log(id);
+        // callback(id);
+      });
+    }); 
+  }  
+  t = process.hrtime(t);
+  console.log('time taken to execute user insertions', t[0], t[1]);
+callback(null, null);
+}
 
+const generateUserData = (callback) => {
+  let users = [];
+  let end = 5000;
+  count = 1;
+  for (var i = 0; i < end; i++) {
+    let user = {
+      username: faker.name.findName(),
+      email: faker.internet.email(),
+      userid: randomstring.generate(12) //faker.random.alphaNumeric(11)
+    };
+    users.push(user);
+  }
+  callback(null, users);
+};
 
+const writeFile = (path, data, opts='utf8') => {
+  console.log('herein write file');
+  new Promise((resolve, reject) => {
+      fs.writeFile(path, data, opts, (err) => {
+          if (err) {
+            reject(err);
+          }  
+          else {
+            resolve();
+          }
+      });
+  })
+  .then(() => {
+    console.log('created file ' + path);
+  });
+};
 
 module.exports = {
-  dg: nameEmailUniqueIdGenerator,
-  videosGenerator: videosGenerator,
-  writeChannelsToFile: writeChannelsToFile
+  gu: generateUsers,
+  // dg: nameEmailUniqueIdGenerator,
+  // videosGenerator: videosGenerator,
+  // writeChannelsToFile: writeChannelsToFile
 };
+
+// var knex = require('knex')(config)
+// var fs = require('fs')
+// var copyFrom = require('pg-copy-streams').from
+
+// knex.client.pool.acquire(function(err, client){
+//   function done (err) {
+//     connection.client.pool.release(client)
+//     if (err) console.log(err)
+//     else console.log('success')
+//   }
+//   var stream = client.query(copyFrom('COPY my_table FROM STDIN'))
+//   var fileStream = fs.createReadStream('some_file.tdv')
+//   fileStream.on('error', done)
+//   fileStream.pipe(stream).on('finish', done).on('error', done)
+// })
